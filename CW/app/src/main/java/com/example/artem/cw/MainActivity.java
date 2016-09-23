e8b6d4bfbb53f12ddd435d9ccc4589e108fb6cab
Romanov Artem <temik_romanov@mail.ru>
2016-09-24 01:23:49 +0300

    Add client and server.

--- CW/app/src/main/java/com/example/artem/cw/MainActivity.java
+++ CW/app/src/main/java/com/example/artem/cw/MainActivity.java
@@ -4,20 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButtonOpen  = null;
+    private Client mServer = null;
+    private static final String LOG_TAG = "myServerApp";
+
 
     ToggleButton toggleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        mButtonOpen = (Button)findViewById(R.id.next);
     }
 
     public void onToggleButtonClick (View view)
@@ -37,9 +43,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+        public void onClick(View view) {
+        /* создаем объект для работы с сервером*/
+        mServer = new Client();
+        /* Открываем соединение. Открытие должно происходить в отдельном потоке от ui */
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mServer.openConnection();
+                    mServer.sendData("Send text to server".getBytes());
+                    mServer.closeConnection();
 
-    public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, Vegetables.class);
-        startActivity(intent);
+                    /*
+                        устанавливаем активные кнопки для отправки данных
+                        и закрытия соедиения. Все данные по обновлению интерфеса должны
+                        обрабатывается в Ui потоке, а так как мы сейчас находимся в
+                        отдельном потоке, нам необходимо вызвать метод  runOnUiThread()
+                    */
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, e.getMessage());
+                    mServer = null;
+                }
+            }
+        }).start();
+
+
+            /* Закрываем соединение */
+
     }
 }
