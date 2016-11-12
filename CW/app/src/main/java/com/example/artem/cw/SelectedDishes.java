a562048d28234dad605228f3ed0991552976ef22
Romanov Artem <temik_romanov@mail.ru>
2016-11-13 01:43:46 +0300

    Сделал создание textView в процессе работы андроид-приложения.

--- CW/app/src/main/java/com/example/artem/cw/SelectedDishes.java
+++ CW/app/src/main/java/com/example/artem/cw/SelectedDishes.java
@@ -1,12 +1,17 @@
 package com.example.artem.cw;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ToggleButton;
 
 /**
@@ -25,18 +30,28 @@ public class SelectedDishes extends AppCompatActivity {
         String[] parts = data.split("/");
         String part1 = parts[0];
         String part2 = parts[1];
-        int i = 0;
-        for (String part : parts )
-        {
-            System.out.println(i + " " + parts[i]);
-            i++;
-        }
+
+        String[] dish1 = part1.split(";");
+        String[] dish2 = part2.split(";");
+
 
 
 
         // Инициализируем компонент
-        TextView textView = (TextView)findViewById(R.id.textView3);
+      //  TextView textView = (TextView)findViewById(R.id.textView3);
+       // textView.setText(data);
+
+
+        RelativeLayout lMain;
+        lMain = (RelativeLayout) findViewById(R.id.selected);
+        RelativeLayout.LayoutParams lParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+        lParams.topMargin = 130;
+
+        TextView textView = new TextView(this);
         textView.setText(data);
+
+        lMain.addView(textView, lParams);
 
     }
 
